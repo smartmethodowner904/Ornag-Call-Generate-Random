412f82de41ae4c01bcfad0c351cccf7db3d2b6af
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 18:06:13 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,12 +1,11 @@
 export const countries = [
-  {
-    name: "ITALY",
-    flag: "🇮🇹",
-    code: "39"
-  },
-  {
-    name: "IRAQ",
-    flag: "🇮🇶",
-    code: "964"
-  }
+  { name: "Italy", code: "+39", flag: "🇮🇹" },
+  { name: "United Kingdom", code: "+44", flag: "🇬🇧" },
+  { name: "Japan", code: "+81", flag: "🇯🇵" },
+  { name: "Pakistan", code: "+92", flag: "🇵🇰" },
+  { name: "Oman", code: "+968", flag: "🇴🇲" },
+  { name: "China", code: "+86", flag: "🇨🇳" },
+  { name: "Qatar", code: "+974", flag: "🇶🇦" },
+  { name: "Bahrain", code: "+973", flag: "🇧🇭" },
+  { name: "Norway", code: "+47", flag: "🇳🇴" }
 ];
