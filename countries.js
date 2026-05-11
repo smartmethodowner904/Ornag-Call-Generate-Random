ca1be92bd28cb5aedff956a1a09d1c57066af0cb
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 15:15:25 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,12 +1,12 @@
 export const countries = [
   {
-    name: "IRAQ",
-    flag: "🇮🇶",
-    code: "964"
-  },
-  {
     name: "ITALY",
     flag: "🇮🇹",
     code: "39"
+  },
+  {
+    name: "IRAQ",
+    flag: "🇮🇶",
+    code: "964"
   }
 ];
