b07044b63eb1c5c462432dcc96fea11dd1da413d
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 22:53:53 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,131 +1,85 @@
 export const countries = [
-
   {
     name: "Italy",
     code: "+39",
     flag: "🇮🇹",
     lang: "it"
   },
+
   {
     name: "United Kingdom",
     code: "+44",
     flag: "🇬🇧",
     lang: "en"
   },
+
   {
     name: "Japan",
     code: "+81",
     flag: "🇯🇵",
     lang: "ja"
   },
+
   {
     name: "Pakistan",
     code: "+92",
     flag: "🇵🇰",
     lang: "ur"
   },
+
   {
     name: "Oman",
     code: "+968",
     flag: "🇴🇲",
     lang: "ar"
   },
+
   {
     name: "China",
     code: "+86",
     flag: "🇨🇳",
     lang: "zh-CN"
   },
+
   {
     name: "Qatar",
     code: "+974",
     flag: "🇶🇦",
     lang: "ar"
   },
+
   {
     name: "Bahrain",
     code: "+973",
     flag: "🇧🇭",
     lang: "ar"
   },
+
   {
     name: "Norway",
     code: "+47",
     flag: "🇳🇴",
     lang: "no"
   },
+
   {
     name: "Indonesia",
     code: "+62",
     flag: "🇮🇩",
     lang: "id"
   },
+
   {
     name: "Singapore",
     code: "+65",
     flag: "🇸🇬",
     lang: "en"
   },
+
   {
     name: "Iran",
     code: "+98",
     flag: "🇮🇷",
     lang: "ja"
-  },
-
-  {
-    name: "India",
-    code: "+91",
-    flag: "🇮🇳",
-    lang: "hi"
-  },
-  {
-    name: "Germany",
-    code: "+49",
-    flag: "🇩🇪",
-    lang: "de"
-  },
-  {
-    name: "France",
-    code: "+33",
-    flag: "🇫🇷",
-    lang: "fr"
-  },
-  {
-    name: "Spain",
-    code: "+34",
-    flag: "🇪🇸",
-    lang: "es"
-  },
-  {
-    name: "Turkey",
-    code: "+90",
-    flag: "🇹🇷",
-    lang: "tr"
-  },
-  {
-    name: "Russia",
-    code: "+7",
-    flag: "🇷🇺",
-    lang: "ru"
-  },
-  {
-    name: "Malaysia",
-    code: "+60",
-    flag: "🇲🇾",
-    lang: "ms"
-  },
-  {
-    name: "Saudi Arabia",
-    code: "+966",
-    flag: "🇸🇦",
-    lang: "ar"
-  },
-  {
-    name: "South Korea",
-    code: "+82",
-    flag: "🇰🇷",
-    lang: "ko"
   }
-
 ];
