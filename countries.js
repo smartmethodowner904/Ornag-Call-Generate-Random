1fb3776ef80953ed4d9659d188b230dc437f3272
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 19:40:56 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,11 +1,94 @@
 export const countries = [
-  { name: "Italy", code: "+39", flag: "🇮🇹" },
-  { name: "United Kingdom", code: "+44", flag: "🇬🇧" },
-  { name: "Japan", code: "+81", flag: "🇯🇵" },
-  { name: "Pakistan", code: "+92", flag: "🇵🇰" },
-  { name: "Oman", code: "+968", flag: "🇴🇲" },
-  { name: "China", code: "+86", flag: "🇨🇳" },
-  { name: "Qatar", code: "+974", flag: "🇶🇦" },
-  { name: "Bahrain", code: "+973", flag: "🇧🇭" },
-  { name: "Norway", code: "+47", flag: "🇳🇴" }
+
+  {
+    name: "Italy",
+    code: "+39",
+    flag: "🇮🇹",
+    lang: "it"
+  },
+
+  {
+    name: "United Kingdom",
+    code: "+44",
+    flag: "🇬🇧",
+    lang: "en"
+  },
+
+  {
+    name: "Japan",
+    code: "+81",
+    flag: "🇯🇵",
+    lang: "ja"
+  },
+
+  {
+    name: "Pakistan",
+    code: "+92",
+    flag: "🇵🇰",
+    lang: "ur"
+  },
+
+  {
+    name: "Oman",
+    code: "+968",
+    flag: "🇴🇲",
+    lang: "ar"
+  },
+
+  {
+    name: "China",
+    code: "+86",
+    flag: "🇨🇳",
+    lang: "zh-CN"
+  },
+
+  {
+    name: "Qatar",
+    code: "+974",
+    flag: "🇶🇦",
+    lang: "ar"
+  },
+
+  {
+    name: "Bahrain",
+    code: "+973",
+    flag: "🇧🇭",
+    lang: "ar"
+  },
+
+  {
+    name: "Norway",
+    code: "+47",
+    flag: "🇳🇴",
+    lang: "no"
+  },
+
+  {
+    name: "Indonesia",
+    code: "+62",
+    flag: "🇮🇩",
+    lang: "id"
+  },
+
+  {
+    name: "Ethiopia",
+    code: "+251",
+    flag: "🇪🇹",
+    lang: "am"
+  },
+
+  {
+    name: "Singapore",
+    code: "+65",
+    flag: "🇸🇬",
+    lang: "en"
+  },
+
+  {
+    name: "Iran",
+    code: "+98",
+    flag: "🇮🇷",
+    lang: "fa"
+  }
+
 ];
